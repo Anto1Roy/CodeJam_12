427847405767dae6c2d614f0c211cc4254ea0115
Anto1Roy <antonin1roy@hotmail.com>
2022-11-19 14:13:27 -0500

    new change css -shrek

--- index.js
+++ index.js
@@ -3,13 +3,11 @@ const themeButton=document.getElementById("themeButton");
 
 const sunnyIcon=document.getElementById("sunnyIcon");
 const coldIcon=document.getElementById("coldIcon");
-const shrekIcon=document.getElementById("shrekIcon");
 
 //this is the possible themes from our css file
 const themeMap = {
     hot: "cold",
-    cold: "shrek",
-    shrek: "hot"
+    cold: "hot"
 };
 
 const theme=localStorage.getItem("theme")||(tmp=Object.keys(themeMap)[0], localStorage.setItem('theme', tmp), tmp);
@@ -20,9 +18,6 @@ switch(theme){
         break;
     case "cold":
         coldIcon.style.display="block";
-        break;
-    case "shrek":
-        shrekIcon.style.display="block";
         break;
 }
 
@@ -38,18 +33,13 @@ function toggleTheme(){
 
     switch (next){
         case "hot":
-            shrekIcon.style.display="none";
+            coldIcon.style.display="none";
             sunnyIcon.style.display="block";
             break;
         case "cold":
             sunnyIcon.style.display="none";
             coldIcon.style.display="block";
             break;
-        case "shrek":
-            coldIcon.style.display="none";
-            shrekIcon.style.display="block";
-            break;
-
     }
     localStorage.setItem("theme", next);
 }
