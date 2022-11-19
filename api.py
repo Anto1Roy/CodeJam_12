73b7f172e5ad0bd3024eb00a86a179f64d2f17ad
baldazzar <andrewstlaurent66@gmail.com>
2022-11-19 11:28:09 -0500

    bruh

--- api.py
+++ api.py
@@ -1,5 +1,6 @@
 import requests
 import json
+from bs4 import BeautifulSoup
 
 URL = "http://api.endlessmedical.com/v1/dx"
 
@@ -9,11 +10,11 @@ def main():
     terms = requests.post(f"{URL}/AcceptTermsOfUse?SessionID={id}&passphrase=I%20have%20read,%20understood%20and%20I%20accept%20and%20agree%20to%20comply%20with%20the%20Terms%20of%20Use%20of%20EndlessMedicalAPI%20and%20Endless%20Medical%20services.%20The%20Terms%20of%20Use%20are%20available%20on%20endlessmedical.com")
     
 
-    cough_lvl = input("Severity of Cough on a scale of 1 to 10")
+    cough_lvl = input("Severity of Cough on a scale of 1 to 10\n")
     cough = requests.post(f"{URL}/UpdateFeature?SessionID={id}&name=SeverityCough&value={cough_lvl}")
     analyze = requests.get(f"{URL}/Analyze?SessionID={id}")
-    print(eval(analyze.content)["Diseases"])
+    for i in range(0,5):
+        print(eval(analyze.content)["Diseases"][i])
 
 if __name__ == '__main__':
-    print("suck my cock")
     main()
